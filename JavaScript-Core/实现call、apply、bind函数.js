6c7b223834deb9814e602e7187a0f0075ba111ee
Lydev68 <18027118545@163.com>
2022-03-15 23:19:50 +0800

    feat:手写实现call/apply/bind函数

--- JavaScript-Core/实现call、apply、bind函数.js
+++ JavaScript-Core/实现call、apply、bind函数.js
@@ -49,9 +49,51 @@
 * 6. 删除刚才添加的属性
 * 7. 返回结果
 * */
+  Function.prototype.myApply = function (context) {
+    // 判断调用对象是否为函数
+    if (typeof this !== 'function') {
+      throw new TypeError('Error')
+    }
+    let result = null;
+    // 判断context是否存在，如果如果未传入则为window
+    context = context || window;
+    // 将函数设置为对象的方法
+    context.fn = this;
+    // 调用方法
+    if (arguments[1]) {
+      result = context.fn(...arguments[1]);
+    } else {
+      result = context.fn();
+    }
+    // 将属性删除
+    delete context.fn;
+    return result
+  }
 
 
 
 /*
 * ============ 3、bind函数的实现步骤 ==============
+*
+* 1. 判断调用对象是否为函数，即使定义在函数的原型上的，但是可能出现使用call等方式调用的情况
+* 2. 保存当前函数的引用，获取其余传入的参数值
+* 3 创建一个函数返回
+* 4. 函数内部使用apply来绑定函数调用，需要判断函数作为构造函数的情况
+* 这个需要传入当前函数的this给apply调用，其余情况都传入指定的上下文对象。
 * */
+  Function.prototype.myBind = function (context) {
+    // 判断调用对象是否为函数
+    if (typeof this !== 'function') {
+      throw new TypeError('Error')
+    }
+    // 获取参数
+    var args = [...arguments].slice(1)
+    var fn = this;
+    return function Fn() {
+      // 根据调用方式，传入不同绑定值
+      return fn.apply(
+        this instanceof Fn ? this : context,
+        args.concat(...arguments)
+      )
+    }
+  }
