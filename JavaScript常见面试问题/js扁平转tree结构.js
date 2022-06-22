6492bc8bba56f558d385faa478444f086b17ce01
Lydever <18027118545@163.com>
2022-06-22 11:05:42 +0800

    feat:jsonp的实现原理

---
+++ JavaScript常见面试问题/js扁平转tree结构.js
@@ -0,0 +1,19 @@
+function list_to_tree(list) {
+  var map = {}, node, roots = [], i;
+
+  for (i = 0; i < list.length; i += 1) {
+    map[list[i].id] = i;      // initialize the map
+    list[i].children = [];    // initialize the children
+  }
+
+  for (i = 0; i < list.length; i += 1) {
+         node = list[i];
+    if (node.parentId !== "0") {
+      // if you have dangling branches check that map[node.parentId] exists
+      list[map[node.parentId]].children.push(node);
+    } else {
+      roots.push(node);
+    }
+  }
+  return roots;
+}
