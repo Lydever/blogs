e094a7f9bd3be0df81d1bf452f1d933337ced994
Lydever <18027118545@163.com>
2022-06-28 14:40:17 +0800

    feat：元组

--- TypeScript-Learning/type.ts
+++ TypeScript-Learning/type.ts
@@ -2,7 +2,7 @@
  * @Author: Lydever 18027118545@163.com
  * @Date: 2022-06-27 15:47:27
  * @LastEditors: Lydever 18027118545@163.com
- * @LastEditTime: 2022-06-27 17:30:20
+ * @LastEditTime: 2022-06-28 14:39:15
  * @FilePath: \LyDevProjects\LyBlogs\TypeScript-Learning\type.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,6 +17,10 @@ let sym:symbol = Symbol('li');
 
 let arr:Array<String> = ["1","2"]
 let arr2:string[] = ["3", "4"];
+
+// 元组
+let x: [string, number]
+let x2 = ['zili', 11]
 
 // 定义联合类型数组
 let arr3: (number | string)[];
