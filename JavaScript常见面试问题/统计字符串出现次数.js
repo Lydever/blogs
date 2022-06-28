7fdf8b93e234de8fa2a71d531f9d07415685fa57
Lydev68 <18027118545@163.com>
2022-06-28 23:28:39 +0800

    feat:js字符串统计问题

--- JavaScript常见面试问题/统计字符串出现次数.js
+++ JavaScript常见面试问题/统计字符串出现次数.js
@@ -14,3 +14,30 @@ for (let j = 0; j < arr.length; j++) {
   }
   console.log(count)
 }
+
+
+/* ======== JS 统计字符串中每一个字符出现的次数 ======== */
+
+var str = 'ABCABC你好你好ssss';
+//用来存储不重复的字符串
+var newStr = '';
+//字符串去重  将不重复的字符串存储到一个新的字符串内
+
+//将每一个字符单独提出来
+for (var i = 0; i < str.length; i++) {
+    //判断有没有在newStr中出现过 没有出现过 放到newStr内
+    if (newStr.lastIndexOf(str[i]) == -1) {
+        newStr += str[i];
+    }
+}
+//  console.log(newStr);//ABC你好の
+for (var i = 0; i < newStr.length; i++) {
+    var count = 0;
+    for (var j = 0; j < str.length; j++) {
+        //判断元素是否相等 如果相等 则次数加1
+        if(newStr[i]==str[j]){
+            count++;
+        }
+    }
+    console.log(newStr[i],count);
+}
