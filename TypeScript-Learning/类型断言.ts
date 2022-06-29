64a9965aade251500e000c09c80dc8574d0c5b78
Lydever <18027118545@163.com>
2022-06-29 09:59:23 +0800

    feat:类型断言

--- TypeScript-Learning/类型断言.ts
+++ TypeScript-Learning/类型断言.ts
@@ -5,6 +5,6 @@
  * @LastEditTime: 2022-06-29 09:53:54
  * @FilePath: \LyDevProjects\LyBlogs\TypeScript-Learning\类型断言.ts
  * @Description: 有时候你会遇到这样的情况，你会比 TypeScript 更了解某个值的详细信息。
- * 通常这会发生在你清楚地知道一个实体具有比它现有类型更确切的类型
+ *               通常这会发生在你清楚地知道一个实体具有比它现有类型更确切的类型
  */
 
