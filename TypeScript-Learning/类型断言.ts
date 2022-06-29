c4f4dd4dafb00929bad02d26eced04fa230f7404
Lydever <18027118545@163.com>
2022-06-29 09:55:52 +0800

    feat:类型断言

--- TypeScript-Learning/类型断言.ts
+++ TypeScript-Learning/类型断言.ts
@@ -4,6 +4,7 @@
  * @LastEditors: Lydever 18027118545@163.com
  * @LastEditTime: 2022-06-29 09:53:54
  * @FilePath: \LyDevProjects\LyBlogs\TypeScript-Learning\类型断言.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 有时候你会遇到这样的情况，你会比 TypeScript 更了解某个值的详细信息。
+ * 通常这会发生在你清楚地知道一个实体具有比它现有类型更确切的类型
  */
 
