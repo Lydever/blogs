e5cb46ee8eaeefc44764924da93c99be9b27b96b
Lydever <18027118545@163.com>
2022-06-29 09:54:41 +0800

    feat:类型断言

---
+++ TypeScript-Learning/类型断言.ts
@@ -0,0 +1,9 @@
+/*  类型断言
+ * @Author: Lydever 18027118545@163.com
+ * @Date: 2022-06-29 09:53:45
+ * @LastEditors: Lydever 18027118545@163.com
+ * @LastEditTime: 2022-06-29 09:53:54
+ * @FilePath: \LyDevProjects\LyBlogs\TypeScript-Learning\类型断言.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
