7ed2060918a52fac0144802ea6ea6433950e0a55
Lydever <18027118545@163.com>
2022-06-29 10:05:58 +0800

    feat:类型断言

--- TypeScript-Learning/类型断言.ts
+++ TypeScript-Learning/类型断言.ts
@@ -7,4 +7,13 @@
  * @Description: 有时候你会遇到这样的情况，你会比 TypeScript 更了解某个值的详细信息。
  *               通常这会发生在你清楚地知道一个实体具有比它现有类型更确切的类型
  */
+const arrayNumber: number[] = [1,2,3,4];
+const arrayNumber2: number = arrayNumber.find(num => num > 2)
+// 其中，greaterThan2 一定是一个数字（确切地讲是 3），因为 arrayNumber 中明显有大于 2 的成员，但静态类型对运行时的逻辑无能为力。
+/* 
+在 TypeScript 看来，greaterThan2 的类型既可能是数字，也可能是 undefined，所以上面的示例中提示了一个 ts(2322) 错误，此时我们不能把类型 undefined 分配给类型 number。
+不过，我们可以使用一种笃定的方式——类型断言（类似仅作用在类型层面的强制类型转换）告诉 TypeScript 按照我们的方式做类型检查。
+*/
+const arrayNumber: number[] = [1,2,3,4];
+const  arrayNumber2: number = arrayNumber.find(num => num > 2) as number;
 
