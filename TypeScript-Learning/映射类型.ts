e7c96432bed80f687720a10d4ce8ecb91998569e
Lydever <18027118545@163.com>
2022-07-01 15:17:07 +0800

    feat:封装DeepPartial

--- TypeScript-Learning/映射类型.ts
+++ TypeScript-Learning/映射类型.ts
@@ -3,3 +3,17 @@
 interface OptionalTinterface<T> = {
     [p in keyof T]+?: T[p]  // 可以通过·+/- 来指定是添加还是删除
 }
+
+// type OptionalInterface = OptionnalInterface<Interface>
+
+// Partial
+// Partial<T> 将类型的属性变成可选
+type Partial<T> = {
+    [P in keyof T]?: T[P];                                                
+}
+
+// Deepartial
+type DeepPartial<T> =  {
+    // 如果是object类型，则递归类型
+    [U in keyof T]?: T[U] extends object ? DeepPartial<T[U]> : [U]
+}
