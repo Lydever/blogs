3088a701e81fa71b8f340f5725f2a827fddb0b67
Lydever <18027118545@163.com>
2022-07-08 14:53:41 +0800

    feat:react hook之useReducer

--- React-Hooks/常用的hook API.js
+++ React-Hooks/常用的hook API.js
@@ -7,3 +7,37 @@ const RenderFunctionComponent = () => {
     <button type="" onClick={() => setState('hello')}>{state}</button>
   );
 }
+
+// useReducer
+// 一个useState的替代方案，就像一个简易的redux
+const initialState = {
+    count: 1
+};
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'increment':
+            return {
+                count: state.count+1
+            }
+            break;
+        case 'decrement':
+            return {
+                count: state.count-1
+            }
+            break;
+        default:
+            throw new Error();        
+    }
+}
+
+const Counter = () => {
+    const [state,dispatch0] = useReducer(reducer,initialState);
+    return (
+        <>
+            Count: {state.count}
+        <button onClick={() => dispatch({type: 'increment'})}>+1</button>
+        <button onClick={() => dispatch({ type: 'decrement' })}>-1</button>
+        </>
+    )
+}
+
