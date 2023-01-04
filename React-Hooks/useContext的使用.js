1738c78b856adbd7715e05ca54e8723a411e172b
Lydever <18027118545@163.com>
2023-01-04 11:07:40 +0800

    useContext的使用

---
+++ React-Hooks/useContext的使用.js
@@ -0,0 +1,37 @@
+/**
+ * @Description:
+ * @Author:   liyingxia
+ * @CreateDate:  2023/1/4 11:06
+ */
+import React, { useContext, useState } from 'react'
+import ReactDOM from 'react-dom'
+
+// define context
+const NumberContext = React.createContext()
+
+const NumberDisplay = () => {
+    const [currentNumber, setCurrentNumber] = useContext(NumberContext)
+
+    const handleCurrentNumberChange = () => {
+        setCurrentNumber(Math.floor(Math.random() * 100))
+    }
+
+    return (
+        <>
+            <div>Current number is: {currentNumber}</div>
+            <button onClick={handleCurrentNumberChange}>Change current number</button>
+        </>
+    )
+}
+
+const ParentComponent = () => {
+    const [currentNumber, setCurrentNumber] = useState({})
+
+    return (
+        <NumberContext.Provider value={[currentNumber, setCurrentNumber]}>
+            <NumberDisplay />
+        </NumberContext.Provider>
+    )
+}
+
+ReactDOM.render(<ParentComponent />, document.getElementById('root'))
